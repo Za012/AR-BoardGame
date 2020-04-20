9a56aaf40e54feb1d78c7c52c321beaf66485bdb
Zeid Adabel <zedjam98@hotmail.com>
2020-04-20 20:32:28 +0200

    Added Game Initialization pattern and AREngine where all the AR related functionality goes (plane detection etc)

---
+++ Assets/Code/Game/LoadInitGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadInitGame : MonoBehaviour
+{
+    void Awake()
+    {
+        Scene levelGame = SceneManager.GetSceneByName("Game");
+        if (!levelGame.isLoaded)
+        {
+            SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
+    }
+}

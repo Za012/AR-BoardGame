05d076dc6624faac63fdacc72c16132b8eeed1ce
Zeid Adabel <zedjam98@hotmail.com>
2020-04-24 19:20:58 +0200

    Implemented SaveFile System

---
+++ Assets/Code-Backend/FileSerializer/SaveFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+[Serializable]
+public class SaveFile
+{
+    private static SaveFile Instance;
+    public GameMetaData MetaData { get; set; }
+
+    private SaveFile()
+    {
+        MetaData = new GameMetaData();
+    }
+    public static void DeleteInstance()
+    {
+        Instance = null;
+    }
+    public static SaveFile LoadInstance()
+    {
+        if (Instance == null)
+        {
+            throw new InvalidOperationException();
+        }
+        return Instance;
+    }
+    public static SaveFile GetInstance()
+    {
+        if (Instance == null)
+        {
+            if (SaveManager.Instance.IsSaveFileCreated())
+            {
+                Instance = SaveManager.Instance.Load();
+                SaveManager.Instance.Save(Instance);
+            }
+            else
+            {
+                Instance = SaveManager.Instance.Save(new SaveFile());
+            }
+        }
+        return Instance;
+    }
+}
---
+++ Assets/Code-Backend/FileSerializer/SaveManager.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager Instance;
+    private SaveManager() { }
+
+    private void Awake()
+    {
+        Instance = GameObject.Find("SaveManager").GetComponent<SaveManager>();
+    }
+
+    private void OnApplicationPause(bool pause)
+    {
+        if (pause)
+        {
+            Debug.Log("Application Paused");
+            Save(SaveFile.GetInstance());
+        }
+    }
+    private void OnApplicationQuit()
+    {
+        Debug.Log("Application Quit");
+        Save(SaveFile.GetInstance());
+    }
+    public bool IsSaveFileCreated()
+    {
+        Debug.Log("Checking path: " + Application.persistentDataPath + "/save.dat");
+        bool isFileCreated = File.Exists(Application.persistentDataPath + "/save.dat");
+        if (isFileCreated)
+        {
+            Debug.Log("SaveFile Exists");
+        }
+        else
+        {
+            Debug.Log("SaveFile Missing");
+        }
+        return isFileCreated;
+    }
+
+    public SaveFile Load()
+    {
+        SaveFile saveFile;
+        Debug.Log("Loading Savefile");
+        using (FileStream fileStream = File.Open(Application.persistentDataPath + "/save.dat", FileMode.Open))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            saveFile = bf.Deserialize(fileStream) as SaveFile;
+            fileStream.Close();
+        }
+        Debug.Log("Loading Complete");
+        return saveFile;
+    }
+
+    public SaveFile Save(SaveFile saveFile)
+    {
+        Debug.Log("Saving Savefile");
+        using (FileStream fileStream = File.Create(Application.persistentDataPath + "/save.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fileStream, saveFile);
+            fileStream.Close();
+        }
+        Debug.Log("Saving Complete");
+        return saveFile;
+    }
+
+    private void DeleteSave()
+    {
+        File.Delete(Application.persistentDataPath + "/save.dat");
+    }
+}

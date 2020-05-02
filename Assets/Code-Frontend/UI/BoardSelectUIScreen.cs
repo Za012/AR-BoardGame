114f6b3e5a36c6f0d3894f9dd0daa1d6b80dfd4b
Zeid Adabel <zedjam98@hotmail.com>
2020-05-02 20:24:44 +0200

    Improved expandability when creating game | Fixed UI duplication bug on certain android devices by adding a camera

--- Assets/Code-Frontend/UI/BoardSelectUIScreen.cs
+++ Assets/Code-Frontend/UI/BoardSelectUIScreen.cs
@@ -5,27 +5,39 @@ using UnityEngine.UI;
 public class BoardSelectUIScreen : BasicUIScreen
 {
     public Text screenLabel;
-    public Button gooseBoard;
+    public Button gooseGameButton;
+    public Button chessGameButton;
     public Text returnText;
     public Text inputField;
-    public Text inputFieldLabel;
+    public Text createGameKeyText;
+
+    public Button openRoomButton;
 
     public override void FillText()
     {
-        gooseBoard.GetComponentInChildren<Text>().text = LanguageManager.Instance.GetWord(gooseBoard.name);
+        gooseGameButton.GetComponentInChildren<Text>().text = LanguageManager.Instance.GetWord(gooseGameButton.name);
+        openRoomButton.GetComponentInChildren<Text>().text = LanguageManager.Instance.GetWord(openRoomButton.name);
+        chessGameButton.GetComponentInChildren<Text>().text = LanguageManager.Instance.GetWord(chessGameButton.name);
+
         screenLabel.text = LanguageManager.Instance.GetWord(screenLabel.name);
+        createGameKeyText.text = LanguageManager.Instance.GetWord(createGameKeyText.name);
         returnText.text = LanguageManager.Instance.GetWord(returnText.name);
         inputField.text = "";
-        inputFieldLabel.text = LanguageManager.Instance.GetWord(inputFieldLabel.name);
     }
     public override void HandleError(string errorName)
     {
         throw new System.NotImplementedException("Error Name: " + errorName + " has not been handled");
     }
+
     public void OnClickGooseGame()
     {
         Game.CURRENTGAMEMETADATA = new GooseGameMetaData();
-        if (!string.IsNullOrEmpty(inputField.text))
+        gooseGameButton.interactable = false;
+    }
+
+    public void OnClickOpenRoom()
+    {
+        if (!string.IsNullOrEmpty(inputField.text) && Game.CURRENTGAMEMETADATA != null)
         {
             Game.NETWORK.OnClickCreateRoom(inputField.text);
         }
